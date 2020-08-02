24bab2445814f4ae83416778f8b327ef362b77b8
armandosae <69097890+armandosae@users.noreply.github.com>
2020-08-02 03:09:28 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-bot.on("ready", function (){
-    console.log('${bot.user.username} Is Online!');
-});
-
-
-bot.login(process.env.token);
