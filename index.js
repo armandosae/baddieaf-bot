d040a105b2ad6c5cdf59769a0c490a0b7768ad48
armandosae <69097890+armandosae@users.noreply.github.com>
2020-08-02 02:45:20 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-worker: node index.js
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+bot.on("ready", function (){
+    console.log('${bot.user.username} Is Online!');
+});
+
+
+bot.login(process.env.token);
