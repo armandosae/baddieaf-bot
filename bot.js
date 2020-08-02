548781fd61550cc90d12db43ee4c28fb03a7a9e3
armandosae <69097890+armandosae@users.noreply.github.com>
2020-08-02 01:23:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NzEyNTU5NjQwMTg5MDc1NDU3.XsTU4A.CYvd6aQftJtnljlICQRcCe_niGw';
-
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on("ready", function (){
+    console.log('${bot.user.username} Is Online!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
