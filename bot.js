e8f31b7a74a2e733b34f081e366634a17f61f57a
armandosae <69097890+armandosae@users.noreply.github.com>
2020-08-01 20:17:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const token = 'NzEyNTU5NjQwMTg5MDc1NDU3.XsTU4A.g07AH7lXPOijHtjC2MZfE011sVQ';
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
